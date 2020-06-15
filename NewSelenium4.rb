6943a39d773c941091f8650c9457c1e9e5c343a5
bhawani singh <chouhanbhawani.es@gmail.com>
2020-06-15 15:58:00 +0530

    created one program and it is right program

--- NewSelenium4.rb
+++ NewSelenium4.rb
@@ -1,8 +1,11 @@
-
+#Program for selenium tutorial task
+# load in the webdriver gem to interact with Selenium
 require 'selenium-webdriver'
+#Run Chrome driver.exe to get chrome browser
+driver = Selenium::WebDriver::Chrome.driver_path = "D:/Eminence_square/chromedriver.exe"
 driver = Selenium::WebDriver.for :chrome
-# driver = Selenium::WebDriver.for :Chrome.driver_path="D:\Eminence_square\chromedriver.exe"
-wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+# use of explicit wait
+wait = Selenium::WebDriver::Wait.new(:timeout => 20)
 
 def document_initialised(driver)
   driver.execute_script('return initialised')
@@ -10,11 +13,16 @@ end
 
 # begin
 driver.get 'https://www.google.com'
+#driver will wait untill locator is not visible
 wait.until{|document_initialised| driver}
 search_form = driver.find_element(:css,"input[name='q']").text
 "Hello from JavaScript!".eql? search_form
+#implicit wait
 driver.manage.window.maximize
-driver.find_element(:css,"input[name='q']").send_keys 'selenium tutorial'
+driver.manage.timeouts.implicit_wait = 20
+
+search=driver.find_element(:css,"input[name='q']")
+search.send_keys 'selenium tutorial'
 sleep(5)
 driver.action.key_down(:shift).send_keys(:arrow_down).perform
 sleep(5)
@@ -24,26 +32,28 @@ sleep(8)
 element1=driver.find_element(:xpath,"(//div[@class='g']//h3)")
 puts"#{element1}"
 for i in 1..10
+  #for loop for scroll the page
   for j in 1..5
     driver.action.key_down(:shift).send_keys(:arrow_down).key_up(:shift).perform
+    j=j+1
   end
+  #if statement for skip i value
   if i==4 ||i==5 || i==6 ||i==7
+
     next
   end
   element=driver.find_element(:xpath,"(//div[@class='g']//h3)[#{i}]")
   sleep(4)
-
+  puts"the title of link is #{element.text}"
   driver.action.move_to(element).perform
   sleep(5)
   #link element locator
-  driver.find_element(:xpath,"(//div[@class='g']//h3)[#{i}]").click
-
-  sleep(10)
-
+  #  driver.find_element(:xpath,"(//div[@class='g']//h3)[#{i}]").click
+  element.click
+  sleep(12)
   driver.navigate.back
-  sleep(10)
+  sleep(12)
   driver.navigate.refresh
   sleep(10)
-
   i+=1
 end
